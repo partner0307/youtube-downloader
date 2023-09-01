456af98a86b7d6e901e6899c5a7a0253f7be211b
ItamiWorld <iris.minami0307@gmail.com>
2023-09-01 19:00:56 +0900

    Create widget

--- index.py
+++ index.py
@@ -3,3 +3,11 @@ from pytube import YouTube
 
 root = Tk()
 root.geometry('500x300')
+root.resizable(0, 0)
+root.title('Youtube Downloader')
+
+link = StringVar()
+
+Label(root, text='Youtube Video Downloader', font='arial 20 bold').pack()
+Label(root, text='Paste Link Here:', font='arial 14').place(x=160, y=60)
+link_enter = Entry(root, width=70, textvariable=link).place(x=32, y=90)
